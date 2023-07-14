7d13ca56795f3aac561a62c977f1bcf845b5350c
Katja <xatja@protonmail.com>
2023-07-14 15:15:26 +0200

    Added public include folder

---
+++ include/jet/events.h
@@ -0,0 +1,10 @@
+#ifndef JET_EVENTS_H_
+#define JET_EVENTS_H_
+
+#include "../../delegate.h"
+#include "../../lazy_singleton.h"
+#include "../../multicast_delegate.h"
+#include "../../observer.h"
+#include "../../subject.h"
+
+#endif
